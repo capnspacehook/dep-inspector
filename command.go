f59039c5fb6fb548e298e68a8901e25938d8903f
Andrew LeFevre <jalefevre@liberty.edu>
2023-09-30 13:39:19 -0400

    print stderr when executing commands fail

--- command.go
+++ command.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -15,14 +17,22 @@ func (d *depInspector) runGoCommand(ctx context.Context, args ...string) error {
 		env = append(env, fmt.Sprintf("%s=%s", envVar, os.Getenv(envVar)))
 	}
 
-	return d.buildCommand(ctx, nil, true, env, args...).Run()
+	cmd, errBuf := d.buildCommand(ctx, nil, env, args...)
+	if err := cmd.Run(); err != nil {
+		return formatCmdErr(err, errBuf)
+	}
+	return nil
 }
 
 func (d *depInspector) runCommand(ctx context.Context, writer io.Writer, args ...string) error {
-	return d.buildCommand(ctx, writer, false, nil, args...).Run()
+	cmd, errBuf := d.buildCommand(ctx, writer, nil, args...)
+	if err := cmd.Run(); err != nil {
+		return formatCmdErr(err, errBuf)
+	}
+	return nil
 }
 
-func (d *depInspector) buildCommand(ctx context.Context, writer io.Writer, stderr bool, env []string, args ...string) *exec.Cmd {
+func (d *depInspector) buildCommand(ctx context.Context, writer io.Writer, env []string, args ...string) (*exec.Cmd, *bytes.Buffer) {
 	var cmd *exec.Cmd
 	if len(args) == 1 {
 		cmd = exec.CommandContext(ctx, args[0])
@@ -30,15 +40,22 @@ func (d *depInspector) buildCommand(ctx context.Context, writer io.Writer, stder
 		cmd = exec.CommandContext(ctx, args[0], args[1:]...)
 	}
 
+	var errBuf bytes.Buffer
 	cmd.Env = env
 	cmd.Stdout = writer
-	if stderr {
-		cmd.Stderr = writer
-	}
+	cmd.Stderr = &errBuf
 
 	if d.verbose {
 		log.Printf("running command: %q", cmd)
 	}
 
-	return cmd
+	return cmd, &errBuf
+}
+
+func formatCmdErr(err error, errBuf *bytes.Buffer) error {
+	var execErr *exec.ExitError
+	if errors.As(err, &execErr) {
+		return fmt.Errorf("%s\n%w", errBuf, err)
+	}
+	return err
 }
